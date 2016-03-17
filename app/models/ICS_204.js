6ca7ab77b8229067dc3951733a3b37a00a655ac6
Jon Mercer <mercer.33@wright.edu>
2016-03-17 07:45:18 -0400

    SD-145 formatted but input does nothing meaningful may not be fully translated

---
+++ app/models/ICS_204.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+const ICS204Schema = new Schema({
+  incidentId: { type: Schema.Types.ObjectId, required: true },
+  operationalPeriod: { 
+    start: Date, 
+    end: Date 
+  },
+  field3: { 
+    branch: String, 
+    division: String, 
+    group: String, 
+    stagingArea: String 
+  },
+  operationsPersonnel: { 
+    operationsSectionChiefName: String, 
+    operationsSectionChiefContactNumber: Number,
+    branchDirectorName: String,
+    branchDirectorContactNumber: Number,
+    divisionGroupSupervisorName: String,
+    divisionGroupSupervisorContactNumber: Number,
+  },
+  resourcesAssigned: [{
+    resourceIdentifier: String, 
+    leader: String, 
+    numOfPersons: Number, 
+    contact: String
+  }],
+  workAssignments: String,
+  specialInstructions: String,
+  communications: [{
+    name: String,
+    function: String,
+    primaryContact: String
+  }],
+  preparedBy: {
+    name: String,
+    positionTitle: String,
+    signature: String,
+    dateTime: Date
+  }
+});
+
+ICS204Schema.statics = {
+  // A helper function to execute a Mongoose query to fetch an Incident by ID.
+  load: function(_id) {
+    return this.findOne({_id})
+      .exec();
+  },
+
+  loadByIncidentId: function(incidentId) {
+    return this.find({incidentId: incidentId})
+      .select('_id')
+      .exec();
+  }
+};
+
+mongoose.model("ICS204", ICS204Schema);
