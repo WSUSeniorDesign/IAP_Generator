44c95efb6338448e10fbade0d792e0ed65e581e2
Jon <themastergeese@gmail.com>
2016-03-26 12:51:53 -0700

    SD-185 complete, but untested

---
+++ app/models/ics206.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ICS206Schema = new Schema({
+  incident: { type: Schema.Types.ObjectId, ref: "Incident", required: true },
+  period: { type: Schema.Types.ObjectId, ref: "Period", required: true },
+  medicalAidStations: [{
+	name: String,
+	location: String,
+	contact: String,
+	paramedics: boolean
+  }],
+  transportation: [{
+	ambulance: String,
+	location: String,
+	contact: String,
+	isLevelALS: boolean
+  }],
+  hospitals: [{
+	name: String,
+	addressLatLong: String,
+	contact: String,
+	airTravelTime: String,
+	groundTravelTime: String,
+	traumaCenterbool: boolean,
+	traumaCenterLevel: Number,
+	burnCenter: boolean,
+	helipad: boolean
+  }],
+  specMedEmerProc: String,
+  preparedBy: {
+    name: String,
+    signature: String
+  }
+  approvedBy: {
+	name: String,
+	signature: String,
+	dateTime: String
+  }
+});
+
+ICS206Schema.statics = {
+  // A helper function to execute a Mongoose query to fetch an Incident by ID.
+  load: function(_id) {
+    return this.findOne({_id})
+      .populate("period")
+      .exec();
+  },
+
+  loadByIncidentId: function(incidentId) {
+    return this.find({incident: incidentId})
+      .select('_id')
+      .exec();
+  },
+
+  loadByPeriodId: function(periodId) {
+    return this.find({period: periodId})
+      .select('_id')
+      .exec();
+  }
+};
+
+mongoose.model("ICS206", ICS206Schema);
